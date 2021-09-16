5b0633bbaa32f8ad59d2c72e05628c5d61cef139
a43414786 <72087693+a43414786@users.noreply.github.com>
2021-09-16 11:25:55 +0800

    Update t.py

--- t.py
+++ t.py
@@ -2,5 +2,23 @@ import pickle
 import matplotlib.pyplot as plt
 with open("losschange.pickle", 'rb') as f:
     data = pickle.load(f)
+epoch = []
+trainLoss = []
+valLoss = []
+valerr = []
 for i in data:
+    epoch.append(i['epoch'])
+    trainLoss.append(i['trainLoss'])
+    valLoss.append(i['valLoss'])
+    valerr.append(i['valerr'])
     print(i)
+
+x = epoch     # Step 1
+y = trainLoss     # Step 1
+fig = plt.figure()            # Step 2
+ax = fig.add_subplot(111)     # Step 3
+plt.plot(x, y, color='blue', linewidth=2, marker='o') # Step 3&4
+plt.xlim(1, 5.5)                   
+plt.savefig('workflow.png')   # Step 5
+plt.show() 
+
